66554ea11a29de1af55b76c716bef15b037c83f4
choi-byoungjin <choibyoungjin85@gmail.com>
2023-04-05 20:31:48 +0900

    [Bronze V] Title: 코딩은 체육과목 입니다, Time: 208 ms, Memory: 17804 KB -BaekjoonHub

---
+++ 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Scanner in = new Scanner(System.in);
+		
+		int N = in.nextInt();
+		String str = "";
+		in.close();
+		
+		for(int i=1; i<=N/4; i++) {
+			str += "long ";
+		}
+		System.out.println(str + "int");
+
+	}
+}
